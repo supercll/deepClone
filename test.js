dc6bb33006fafc8a489a0c5b8d0cdbbb24fa8065
lc <1033581609@qq.com>
2020-10-01 16:28:25 +0800

    编写测试集

---
+++ test.js
@@ -0,0 +1,16 @@
+const obj = {
+    simple: 1,
+    obj: { a: 1 },
+    arry: [1, 2, 3],
+    func: function (x) {
+        console.log(x);
+    },
+    date: new Date(),
+    reg: /^(object|function)$/,
+    symbol: Symbol("key"),
+    undefined: undefined,
+    null: null,
+    circle: {
+        self: obj,
+    },
+};
